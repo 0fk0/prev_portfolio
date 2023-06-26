5ee3f577bc4d151f62624eadd22253bf98676b50
0fk0 <greeeen_ken41ei39jin@icloud.com>
2023-06-27 01:57:18 +0900

    modify swiper function

--- scripts/main.js
+++ scripts/main.js
@@ -20,35 +20,3 @@ btn_c.addEventListener('click',{tag:contact, handleEvent:scrollWindow});
 // ページ再読み込み
 const html = "html";
 window.addEventListener('load', {tag:html, handleEvent:scrollWindow});
-
-// WORKS
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
-    effect: 'coverflow',
-    speed: 1000,
-    allowTouchMove: false,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-});
-
-// クリック時遷移
-function mvPage(link){
-  window.open(this.link);
-}
-// 具体的な遷移対象
-const musicAlert = window.document.querySelector(".music-alert");
-const memoBot = window.document.querySelector(".memo-bot");
-musicAlert.addEventListener('click', {link:"./webapp/MusicAlert/home.html", handleEvent:mvPage});
-memoBot.addEventListener('click', {link:"https://github.com/0fk0/discord_memobot", handleEvent:mvPage});
